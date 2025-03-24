01897a9c05de2f31742694c9e0cd0815d71e468e
Eder-F <117577648+Eder-F@users.noreply.github.com>
2025-03-24 16:45:07 +0000

    Add files via upload

---
+++ FundamentosComputacionais/teste02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int numeroUm, numeroDois, soma;
+
+ printf("Digite um numero ");
+ scanf ("%d",&numeroUm);
+
+ printf("Digite outro numero ");
+ scanf ("%d",&numeroDois);
+
+ soma = numeroUm+numeroDois;
+ printf("A soma dos numeros e igual a %d ", soma);
+ 
+ return 0;
+
+
+}
